ba5bdb9b3e1992e270ef5514feed91781e862ebf
laureanoperez-svg <laureanoperez@institutocedec.com>
2026-06-09 15:05:22 -0300

    Formulario Modal

---
+++ modelos/productos.js
@@ -0,0 +1,41 @@
+export const productos = [
+    {
+        codigo: 103,
+        nombre: 'iPhone SE',
+        descripcion: {
+            procesador: 'A13 Bionic',
+            almacenamiento: '128 GB',
+            camaras: '7 Mp / 12 Mp',
+            pantalla: '4,7 IPS Retina',
+        },
+        precio: 1749999,
+        imagen: 'iPhone-SE-Black.jpg',
+        categoria: 'Telefonía'
+    },
+    {
+        codigo: 102,
+        nombre: 'Motorola Moto G-100',
+        descripcion: {
+            procesador: 'Octa Core (3,2 GHz)',
+            almacenamiento: '128 GB',
+            camaras: '13 Mp / 64 Mp',
+            pantalla: '6,7 FHD+ LTPS',
+        },
+        precio: 749999,
+        imagen: 'Motorola-Moto-G100.jpg',
+        categoria: 'Telefonía'
+    },
+    {
+        codigo: 104,
+        nombre: 'iPhone 11 Pro Max',
+        descripcion: {
+            procesador: 'Chip A13 Bionic',
+            almacenamiento: '256 GB',
+            camaras: '12 Mp / 12 Mp (Triple)',
+            pantalla: '5,8 Super Retina XDR - OLED Multi - Touch'
+        },
+        precio: 23289900,
+        imagen: 'Iphone-11-Pro-Max.jpg',
+        categoria: 'Telefonía'
+    }
+]
