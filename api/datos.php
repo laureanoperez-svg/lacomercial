c8cdbe2aeeb0e3e42d7a9fb6f48406a4810cdc54
laureanoperez-svg <laureanoperez@institutocedec.com>
2026-08-25 15:08:05 -0300

    Add files via upload

---
+++ api/datos.php
@@ -0,0 +1,39 @@
+<?php
+// Requerimos el archivo modelos.php
+require_once 'modelos.php';
+// Si hay en parámetro tabla
+if(isset($_GET['tabla'])) { // Si está seteado el parámetro tabla
+    $tabla = new Modelo($_GET['tabla']); // Creamos el objeto tabla
+
+    if(isset($_GET['accion'])) {
+        if($_GET['accion'] == 'insertar') {
+            $valores = $_POST;
+        }
+
+        switch($_GET['accio']) {
+            case 'seleccionar':
+                $datos = $tabla->seleccionar(); // Ejecutamos el método seleccionar
+                echo $datos; // Mostramos los datos
+                break;
+            case 'insertar':
+                $id = $tabla->insertar($valores);
+
+                if($id > 0) {
+                    $respuesta = [
+                        'success' => true,
+                        'message' => 'Registro insertado correctamente',
+                        'id' => $id
+                    ];
+                } else {
+                    $respuesta = [
+                        'success' => false,
+                        'message' => 'Error al insertar el registro'
+                    ];
+                }
+
+                echo json_encode($respuesta);
+                break;
+        }
+    }
+}
+?>
